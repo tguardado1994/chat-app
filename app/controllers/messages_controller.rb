7eeb1ddb354ee70a34d973f72660e0279779d5e2
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-03-02 18:53:57 -0600

    edit
    edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,14 @@
 class MessagesController < ApplicationController
-    def create 
-        @current_user = current_user
-        @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
+   
+  def create
+    @current_user = current_user
+    @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
+  end
 
-    end
+  private
 
-
-    private
-
-    def msg_params
-        params.require(:message).permit(:content)
-    end
+  def msg_params
+    params.require(:message).permit(:content)
+  end
 
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -18,7 +18,10 @@ class RoomsController < ApplicationController
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
     @room = Room.new
-    @message = Message.new 
+    @message = Message.new
+
+    @messages = @single_room.messages
+
     render "index"
   end
 end
